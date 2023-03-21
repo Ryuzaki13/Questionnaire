2af8e2baeb9610758008571f7e43ed786e902462
Ryuzaki13 <ryuzaki_dk_rm_guitar@mail.ru>
2023-03-21 22:46:59 +0500

    Добавлен элемент для отображения информации или ошибки.

--- Questionnaire/MainWindow.xaml.cs
+++ Questionnaire/MainWindow.xaml.cs
@@ -1,22 +1,33 @@
-﻿using System.Windows;
-using System.Text.Json;
-using QuestionnaireDatabaseLib;
-using System.Text.Json.Nodes;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
-using System;
+using System.Windows.Media;
+using System.Windows.Threading;
+using QuestionnaireDatabaseLib;
 
 namespace Questionnaire {
 
 	public partial class MainWindow : Window {
+		private static DispatcherTimer timer = new DispatcherTimer();
+		private static Border border;
+		private static TextBlock textBlock;
+		private static SolidColorBrush errorColor = new SolidColorBrush(Color.FromArgb(150, 150, 0, 0));
+		private static SolidColorBrush noticeColor = new SolidColorBrush(Color.FromArgb(150, 0, 150, 0));
+
 		public MainWindow() {
 			InitializeComponent();
 
+			timer.Interval = new TimeSpan(0, 0, 0, 5);
+			timer.Tick += Timer_Tick;
+			border = Message;   // Имя элемента <Border>
+			textBlock = MessageText; // Имя элемента <TextBlock>
+
 			ConnectionParameters parameters = ConnectionParameters.Load("connection_string.json");
-			if (parameters == null ) {
+			if (parameters == null) {
 				parameters = new ConnectionParameters("127.0.0.1", 5432, "postgres", "1234", "Questionnaire");
 				parameters.Save();
 			}
-				
+
 			Connection connection = new Connection(new ConnectionParameters() {
 				Database = "Questionnaire",
 				Host = "localhost",
@@ -27,7 +38,26 @@ namespace Questionnaire {
 
 			Cache.Connection = connection;
 
+			
+			//AppFrame.Navigate(View.Pages.Registration);
 			AppFrame.Navigate(View.Pages.Authorization);
+		}
+
+		public static void MessageShow(string message, bool isError = false) {
+			textBlock.Text = message;
+			if (isError) {
+				border.Background = errorColor;
+			} else {
+				border.Background = noticeColor;
+			}
+			border.Visibility = Visibility.Visible;
+			timer.Start();
+		}
+
+		private void Timer_Tick(object sender, EventArgs e) {
+			MessageText.Text = "";
+			Message.Visibility = Visibility.Collapsed;
+			timer.Stop();
 		}
 	}
 }
